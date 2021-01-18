a36b375f6b5df48e7344285d354529ea9a6a8b6d
nolbo <jun9975sun@gmail.com>
2021-01-18 14:28:15 +0900

    Release

---
+++ script/href.js
@@ -0,0 +1,42 @@
+$(function() {
+    $('h4.con_txt').click(function() {
+        location.href = `${$(this).attr('id').replace(/_/g, '/')}/index.html`;
+    });
+
+    $('h4.name').click(function() {
+        if(location.pathname === '/index.html' || location.pathname === '/') {
+            const parent = $(this).parent();
+
+            location.href = `${parent.attr('data-folder')}/${parent.attr('id')}/${$(this).text().toLowerCase()}/index.html`
+        }
+    });
+
+    $('h5.path').click(function() {
+        if(location.pathname === '/index.html' || location.pathname === '/') {
+            location.href = `${$(this).parent().attr('data-folder').replace(/_/g, '/')}#${$(this).text().toLowerCase()}`;
+        } else {
+            location.href = `../${$(this).text().toLowerCase()}/index.html`;
+        }
+    });
+
+    $('i[data-link = "true"]').click(function(e) {
+        location.href = `${$(this).attr('id')}/index.html`;
+    });
+
+    $('strong.link, code[data-link = "true"]').click(function() {
+        if($(this).attr('data-nottab') === 'true') {
+            location.href = $(this).attr('id');
+        } else {
+            window.open($(this).attr('id'));           
+        }
+    });
+
+    $('h2.title').click(function() {
+        if(location.pathname.split('/').length === 5) {
+            location.href = location.origin;
+        } else {
+            location.href = '../';
+        }
+        
+    })
+});
